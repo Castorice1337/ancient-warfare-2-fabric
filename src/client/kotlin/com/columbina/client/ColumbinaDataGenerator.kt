1c18700fdba597c6413f1414b4c3cca0a33306ad
Castorice1337 <251271507+Castorice1337@users.noreply.github.com>
2026-04-10 20:43:57 +0800

    Initial Fabric mod scaffold

---
+++ src/client/kotlin/com/columbina/client/ColumbinaDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.columbina.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ColumbinaDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
